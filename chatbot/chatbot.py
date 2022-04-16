ee931f5515a9a77dbfe3aa212a393d0ff6fc5cf7
Maximilian Krahn <maximilian.krahn@icloud.com>
2022-04-16 20:14:33 +0300

    added BOT

---
+++ chatbot/chatbot.py
@@ -0,0 +1,29 @@
+from telegram.ext import Updater
+updater = Updater(token='TOKEN', use_context=True)
+
+dispatcher = updater.dispatcher
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+from telegram import Update
+from telegram.ext import CallbackContext
+
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
+
+from telegram.ext import MessageHandler, Filters
+
+def echo(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Infered Message')
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
